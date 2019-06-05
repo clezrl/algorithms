9ca072b974bddc690d15e9438b7f3304644b93c4
clezrl <cintra.chris@gmail.com>
2019-06-05 14:51:16 -0400

    Bubble Sort lab from 6/4

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -1,19 +1,21 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "sorts.hpp"
 
+//O(1), Omega(1), Theta(1)
 void swap(int* a, int* b) {
   int temp = *a;
   *a = *b;
   *b = temp;
 }
 
+//O(n^2), Omega(n)
 void bubble_sort(int* arr, int n) {
   bool sorted = false;
   while (!sorted) {
     sorted = true;
-    for (int j = 0; j < n - 1; ++j) {
-      if (arr[j] > arr[j + 1]) {
-        swap(arr + j, arr + j + 1);
+    for (int i = 0; i < n - 1; ++i) {
+      if (arr[i] > arr[i + 1]) {
+        swap(&arr[i], &arr[i + 1]);
         sorted = false;
       }
     }
